ccac281e3fddcb6d74103d39ee1ed00674ffca22
Aditya Hriday Sahu <f20200144@goa.bits-pilani.ac.in>
2022-06-06 20:55:03 +0530

    A Simple Storage Project Using Brownie

---
+++ scripts/deploy.py
@@ -0,0 +1,29 @@
+from brownie import accounts, config, SimpleStorage, network
+
+
+def deploy_simple_storage():
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
+    # SimpleStorage.wait(1)
+    # account = accounts.load("rinkeby-acc-2")
+    # print(account)
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
+    # print(simple_storage)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def main():
+    deploy_simple_storage()
